b6b9bd648b73722345a1ac08a9e4eac0c37eae12
mtennekes <mtennekes@gmail.com>
2025-09-08 14:46:36 +0200

    ...

--- R/tm_mapbox.R
+++ R/tm_mapbox.R
@@ -2,11 +2,10 @@
 #'
 #' Mapbox mode options. These options are specific to the mapbox mode.
 #'
-#' @param style To do: migrate with tmap styles and basemaps?
 #' @param pitch The pitch angle
 #' @example examples/mapbox.R
 #' @export
-tm_mapbox = function(style, pitch) {
+tm_mapbox = function(pitch) {
 	args = lapply(as.list(rlang::call_match(dots_expand = TRUE)[-1]), eval, envir = parent.frame())
 	args$called_from = "tm_mapbox"
 	do.call(tmap::tm_options, args)
@@ -16,11 +15,10 @@ tm_mapbox = function(style, pitch) {
 #'
 #' Maplibre mode options. These options are specific to the maplibre mode.
 #'
-#' @param style To do: migrate with tmap styles and basemaps?
 #' @param pitch The pitch angle
 #' @example examples/maplibre.R
 #' @export
-tm_maplibre = function(style, pitch) {
+tm_maplibre = function(pitch) {
 	args = lapply(as.list(rlang::call_match(dots_expand = TRUE)[-1]), eval, envir = parent.frame())
 	args$called_from = "tm_maplibre"
 	do.call(tmap::tm_options, args)
